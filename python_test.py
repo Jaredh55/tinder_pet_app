d969bdce2ff2c222d8bda310d3057784159e4d1b
Jaredh55 <jaredh55@gmail.com>
2018-09-06 19:36:55 -0500

    initial

---
+++ python_test.py
@@ -0,0 +1,8 @@
+
+a = 14
+b = 17
+
+if a > b:
+  print("14 is greater than 17")
+elif b > a:
+  print("17 is greater than 14")
---
+++ test_app.py
@@ -0,0 +1,100 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+users = [
+          {
+            "name": "steve",
+            "age": 42,
+            "title": "engineer"
+          },
+          {
+            "name": "phil",
+            "age": 32,
+            "title": "salesman"
+          },
+          {
+            "name": "sarah",
+            "age": 32,
+            "title": "COO"
+          }
+
+]
+
+class User(Resource):
+
+  def get(self, name):
+    for user in users:
+      if(name == user["name"]):
+        return user, 200
+    return "User not found", 404
+
+  def post(self, name):
+    parser = reqparse.RequestParser()
+    parser.add_argument("age")
+    parser.add_argument("title")
+    args = parser.parse_args()
+
+    for user in users:
+      if(name == user["name"]):
+        return "User with name {} already exists".format(name), 400
+
+    user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+    }
+    users.append(user)
+    return user, 201
+
+  def put(self, name):
+    parser = reqparse.RequestParser()
+    parser.add_argument("age")
+    parser.add_argument("title")
+    args = parser.parse_args()
+
+    for user in users:
+      if(name == user["name"]):
+        user["age"] = args["age"]
+        user["occupation"] = args["occupation"]
+        return user, 200
+
+    user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+    }
+    users.append(user)
+    return user, 201
+
+  def delete(self, name):
+    global users
+    users = [user for user in users if user["name"] != name]
+    return "{} is deleted.".format(name), 200
+
+
+api.add_resource(User, "/user/<string:name>")
+app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
